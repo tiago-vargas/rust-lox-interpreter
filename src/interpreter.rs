cdcd325bd03ce6dcb14510f3fd8b3b9e7892e758
Tiago Vargas <tiago.ce10@outlook.com>
2023-08-27 00:45:27 -0300

    Scan ")"

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -3,10 +3,30 @@ use std::fmt::Debug;
 struct Scanner;
 
 impl Scanner {
-    fn scan_tokens(source: &str) -> &[Token] {
-        &[Token {
-            r#type: Type::OpenParen,
-        }]
+    fn scan_tokens(source: &str) -> Vec<Token> {
+        let mut tokens: Vec<Token> = vec![];
+
+        for byte in source.as_bytes() {
+            Self::scan_token(*byte, &mut tokens);
+        }
+
+        tokens
+    }
+
+    fn scan_token(byte: u8, tokens: &mut Vec<Token>) {
+        match &[byte] {
+            b"(" => {
+                tokens.push(Token {
+                    r#type: Type::OpenParen,
+                });
+            }
+            b")" => {
+                tokens.push(Token {
+                    r#type: Type::CloseParen,
+                });
+            }
+            _ => todo!()
+        }
     }
 }
 
@@ -22,13 +42,14 @@ impl PartialEq for Token {
 
 impl Debug for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		write!(f, "«{:?}»", self.r#type)
+        write!(f, "«{:?}»", self.r#type)
     }
 }
 
 #[derive(Debug, PartialEq)]
 enum Type {
     OpenParen,
+    CloseParen,
 }
 
 #[cfg(test)]
@@ -45,6 +66,20 @@ mod tests {
             tokens,
             &[Token {
                 r#type: Type::OpenParen
+            }]
+        )
+    }
+
+    #[test]
+    fn scans_close_paren() {
+        let code = ")";
+
+        let tokens = Scanner::scan_tokens(code);
+
+        assert_eq!(
+            tokens,
+            &[Token {
+                r#type: Type::CloseParen
             }]
         )
     }
