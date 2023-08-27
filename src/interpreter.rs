6dd4032d63215caed8119882f668e333dcdcbb5b
Tiago Vargas <tiago.ce10@outlook.com>
2023-08-27 01:31:33 -0300

    Fix style

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -18,29 +18,15 @@ impl Scanner {
 
     fn identify_token(byte: u8) -> Token {
         match &[byte] {
-            b"(" => Token {
-                r#type: Type::LeftParen,
-            },
-            b")" => Token {
-                r#type: Type::RightParen,
-            },
-            b"{" => Token {
-                r#type: Type::LeftBrace,
-            },
-            b"}" => Token {
-                r#type: Type::RightBrace,
-            },
-            b"," => Token {
-                r#type: Type::Comma,
-            },
+            b"(" => Token { r#type: Type::LeftParen },
+            b")" => Token { r#type: Type::RightParen },
+            b"{" => Token { r#type: Type::LeftBrace },
+            b"}" => Token { r#type: Type::RightBrace },
+            b"," => Token { r#type: Type::Comma },
             b"." => Token { r#type: Type::Dot },
-            b"-" => Token {
-                r#type: Type::Minus,
-            },
+            b"-" => Token { r#type: Type::Minus },
             b"+" => Token { r#type: Type::Plus },
-            b";" => Token {
-                r#type: Type::Semicolon,
-            },
+            b";" => Token { r#type: Type::Semicolon },
             b"*" => Token { r#type: Type::Star },
             _ => todo!(),
         }
@@ -60,29 +46,15 @@ mod tests {
         assert_eq!(
             tokens,
             &[
-                Token {
-                    r#type: Type::LeftParen
-                },
-                Token {
-                    r#type: Type::RightParen
-                },
-                Token {
-                    r#type: Type::LeftBrace
-                },
-                Token {
-                    r#type: Type::RightBrace
-                },
-                Token {
-                    r#type: Type::Comma
-                },
+                Token { r#type: Type::LeftParen },
+                Token { r#type: Type::RightParen },
+                Token { r#type: Type::LeftBrace },
+                Token { r#type: Type::RightBrace },
+                Token { r#type: Type::Comma },
                 Token { r#type: Type::Dot },
-                Token {
-                    r#type: Type::Minus
-                },
+                Token { r#type: Type::Minus },
                 Token { r#type: Type::Plus },
-                Token {
-                    r#type: Type::Semicolon
-                },
+                Token { r#type: Type::Semicolon },
                 Token { r#type: Type::Star },
             ],
             r#"Did not scan "(){{}},.-+;*""#
